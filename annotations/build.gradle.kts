bf3d782fd3aa892fa48f2a5a3a2ff7eef56c0751
marti <martim.ramos2005@gmail.com>
2026-04-20 12:04:23 +0100

    Criação da classe Extract no modulo annotations que implementa o corpo da anotação utilizada.

--- annotations/build.gradle.kts
+++ annotations/build.gradle.kts
@@ -11,7 +11,7 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
-    implementation(kotlin("stdlib")) // biblioteca que contém as ferramentas essenciais para o Kotlin trabalhar
+    implementation(kotlin("stdlib")) // biblioteca que contém as ferramentas essenciais para o Kotlin trabalhar. Apesar de a partir da versão 1.4 do Kotlin, saída a 2020, esta biblioteca já vem incorporada nos projetos Kotlin através do plug-in "kotlin("jvm")". Sendo assim , é mantida a mesma linha de código para reforçar que esta bilbioteca já exisitia por default no projeto mas assim temos a noção do seu uso e para que serve
 }
 
 kotlin {
---
+++ annotations/src/main/kotlin/annotations/Extract.kt
@@ -0,0 +1,4 @@
+package annotations
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.SOURCE)
+annotation class Extract(val regex: String)
